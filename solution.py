67490ca545d42d897480a752a4e81420f22732d0
Matthew Heeter <matthewheeter1611@gmail.com>
2023-04-23 22:15:25 -0400

    Added array of arrays of ones

---
+++ solution.py
@@ -0,0 +1,8 @@
+n = 5
+
+def pascTri(numRows):
+    # Need to create an array containing numRows # of arrays containing a 1
+    arr = [[1] for i in range(numRows)]
+    #for i in range(n):
+    return arr
+print(pascTri(n))
