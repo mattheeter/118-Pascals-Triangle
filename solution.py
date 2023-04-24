8074b6b80ffb60161f4254c448c12b6b54b451fe
Matthew Heeter <matthewheeter1611@gmail.com>
2023-04-23 22:38:56 -0400

    Array of arrays of ones with correct length

--- solution.py
+++ solution.py
@@ -1,8 +1,21 @@
 n = 5
 
 def pascTri(numRows):
-    # Need to create an array containing numRows # of arrays containing a 1
-    arr = [[1] for i in range(numRows)]
-    #for i in range(n):
-    return arr
+    # Need to create an array containing numRows # of arrays containing all ones
+    tri = [[1]*(i+1) for i in range(numRows)] # [x]*y syntax : https://intellipaat.com/community/63426/how-to-create-a-zero-matrix-without-using-numpy
+    # Adding up the two upper numbers to get the lower number (starting at third array bc first two are only ones)
+    #for n in tri[2:]:
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return tri
 print(pascTri(n))
---
+++ test.py
@@ -0,0 +1,2 @@
+tri = list(range(1,20,1))
+print(tri)
